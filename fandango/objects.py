a6f8e690db4db6e3dbdfb00dd51562a222285e10
sergi_rubio <sergi_rubio@4e9c00fd-8f2e-0410-aa12-93ce3db5e235>
2010-12-01 09:40:33 +0000

    objects: Added Pool ; a class for managing queued-threaded tasks
    git-svn-id: https://svn.code.sf.net/p/tango-cs/code/fandango/trunk@8402 4e9c00fd-8f2e-0410-aa12-93ce3db5e235

--- fandango/objects.py
+++ fandango/objects.py
@@ -42,6 +42,7 @@
 
 from __builtin__ import object
 from functional import *
+from operator import isCallable
 
 ## Useful class objects
 
@@ -65,7 +66,23 @@ def Property(fget=None,fset=None,fdel=None,doc=None):
     MyObject.X = Property(fget,fset,fdel,'X')        
     """
     return property(partial(fget,var=doc) if fget else None,partial(fset,var=doc) if fset else None,partial(fdel,var=doc) if fdel else None,doc=doc)
-
+    
+import threading
+__lock__ = threading.RLock()
+def locked(f,*args,**kwargs):
+    """ 
+    decorator for secure-locked functions 
+    A key-argument _lock can be used to use a custom Lock object
+    """
+    _lock = kwargs.pop('_lock',__lock__)
+    try:
+        _lock.acquire()
+        return f(*args,**kwargs)
+    except Exception,e:
+        print 'Exception in%s(*%s,**%s): %s' % (f.__name__,args,kwargs,e)
+    finally:
+        _lock.release()
+            
 def self_locked(func,reentrant=True):
     ''' Decorator to make thread-safe class members
     @deprecated
@@ -75,7 +92,6 @@ def self_locked(func,reentrant=True):
         With Lock() this decorator should not be used to decorate nested functions; it will cause Deadlock!
         With RLock this problem is avoided ... but you should rely more on python threading.
     '''
-    import threading
     def lock_fun(self,*args,**kwargs):
         if not hasattr(self,'lock'):
             setattr(self,'lock',threading.RLock() if reentrant else threading.Lock())
@@ -90,6 +106,116 @@ def self_locked(func,reentrant=True):
             #if self.trace: print "released: %s"%self.lock
         return result       
     return lock_fun
+
+import Queue
+class Pool(object):
+    """ 
+    It creates a queue of tasks managed by a pool of threads.
+    Each task can be a Callable or a Tuple containing args for the "action" class argument.
+    If "action" is not defined the first element of the tuple can be a callable, and the rest will be arguments
+    
+    Usage:
+        p = Pool()
+        for item in source(): p.add_task(item)
+        p.start()
+        while len(self.pending()):
+            time.sleep(1.)
+        print 'finished!'
+    """
+    
+    def __init__(self,action=None,max_threads=5,start=False,mp=False):   
+        import threading
+        if mp==True:
+            import multiprocessing
+            self._myThread = multiprocessing.Process
+            self._myQueue = multiprocessing.Queue
+        else:
+            import Queue
+            self._myThread = threading.Thread
+            self._myQueue = Queue.Queue
+        self._action = action
+        self._max_threads = max_threads
+        self._threads = []
+        self._pending = []
+        self._stop = threading.Event()
+        self._lock = threading.Lock()
+        self._locked = partial(locked,_lock=self._lock)
+        self._started = start
+        self._queue = self._myQueue()
+        
+    def start(self):
+        """ 
+        Start all threads.
+        """
+        [t.start() for t in self._threads]
+        self._started = True
+        
+    def stop(self):
+        self._stop.set()
+        [t.join(3.) for t in self._threads]
+        #while not self._queue.empty(): self._queue.get()
+        self._retire()
+        self._started = False
+        
+    def add_task(self,item):#,block=False,timeout=None):
+        """
+        Adds a new task to the queue
+        :param task: a callable or a tuple with callable and arguments
+        """
+        self._locked(self._pending.append,str(item))
+        if self._started: self._retire()
+        if len(self._pending)>len(self._threads) and len(self._threads)<self._max_threads:
+            self._new_worker()        
+        self._queue.put(item)#,block,timeout)
+
+    def pending(self):
+        """ returns a list of strings with the actions not finished yet"""
+        self._retire()
+        return self._pending
+        
+    ####################################################################################
+    #Protected methods
+            
+    def _new_worker(self):
+        #Creates a new thread
+        t = self._myThread(target=self._worker)
+        self._locked(self._threads.append,t)
+        t.daemon = True
+        if self._started: t.start()      
+        
+    def _retire(self):
+        #Cleans dead threads
+        dead = [t for t in self._threads if not t.is_alive()]
+        for t in dead:
+            self._locked(self._threads.remove,t) 
+    
+    def _worker(self):
+        #Processing queue items
+        while not self._stop.isSet() and not self._queue.empty():
+            item = self._queue.get()
+            try:
+                if item is not None and isCallable(item): 
+                    item()
+                elif isSequence(item): 
+                    if self._action: self._action(*item)
+                    elif isCallable(item[0]): item[0](*item[1:])
+                elif self._action: 
+                    self._action(item)
+            except:
+                import traceback
+                print('objects.Pool.worker(%s) failed: %s'%(str(item),traceback.format_exc()))
+            self._remove_task(item)
+        return
+                
+        
+    def _remove_task(self,item=None):
+        #Remove a finished task from the list
+        if str(item) in self._pending: 
+            self._locked(self._pending.remove,str(item))
+        return getattr(self._queue,'task_done',lambda:None)()
+            
+    pass
+    
 
 class Singleton(object):
     """This class allows Singleton objects overriding __new__ and renaming __init__ to init_single
