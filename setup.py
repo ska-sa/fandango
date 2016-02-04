31b7241f12a3074e7cab1866a2ed653cf927af64
Neilen Marais <nmarais@ska.ac.za>
2016-02-04 11:08:51 +0200

    Get setup.py version from CHANGES file

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
+import os, imp
+
 from setuptools import setup, find_packages
+
+ReleaseNumber = type('ReleaseNumber',(tuple,),
+                     {'__repr__':(lambda self:'.'.join(('%02d'%i for i in self)))})
+release = ReleaseNumber(
+    imp.load_source('changelog',os.path.join('.', 'fandango', 'CHANGES'))
+    .RELEASE)
 
 setup(
     name="fandango",
-    version="0.1",
-    packages = find_packages(),
+    version=str(release),
+    packages=find_packages(),
     description="Simplify the configuration of big Tango control systems",
     long_description="Simplify the configuration of big Tango control systems; "
     "implementing the behavior of Jive (configuration) and/or Astor (deployment) "
     "tools in methods that could be called from scripts using regexp and "
     "wildcards.",
     author = "Sergi Rubio",
-    author_email = "srubio@cells.es",
+    author_email="srubio@cells.es",
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Other Environment',
